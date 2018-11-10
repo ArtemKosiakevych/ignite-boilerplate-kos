a51c82c62488aaa244917c79daedfd86896cff6b
ArtemKosiakevych <artem.kosiakevych@gmail.com>
2018-11-10 20:59:27 +0200

    first commit

---
+++ boilerplate/App/Containers/LaunchScreen.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import { View } from 'react-native'
+
+// Styles
+import styles from './Styles/LaunchScreenStyles'
+
+export default class LaunchScreen extends Component {
+  render() {
+    return <View style={styles.mainContainer} />
+  }
+}
